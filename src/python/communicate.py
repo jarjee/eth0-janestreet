7c8993ebade05f6bd6bbeb22306b38bceb44bfa9
Nathan Smyth <nathan@foodbanklocal.com>
2015-03-14 10:14:52 +1100

    Adding stubs for communication to the server

---
+++ src/python/communicate.py
@@ -0,0 +1,34 @@
+import json
+
+__hasConnected = False;
+__team_name = "Carbon 14"
+
+def connectionCheck():
+    if (! __hasConnected):
+        print("Have not connected to the server yet!")
+        exit(1)
+
+def connect():
+    #Send {"type": "hello", "team": "Carbon 14"}
+    __hasConnected = True;
+    pass
+
+#With an already established connection, add an order
+def add(order_id, com, price, size):
+    #Symbol is team name
+    connectionCheck()
+    #Send {"type": "add", "order_id": N, "symbol": "SYM", "dir": "BUY", "price": N, "size": N}
+    pass
+
+#Converting an order from buy to sell, change size, symbol etc
+def convert(order_id, com, size):
+    #Symbol is team name
+    connectionCheck()
+    #Send {"type": "convert", "order_id": N, "symbol": "SYM", "dir": "BUY", "size": N}
+    pass
+
+#Cancel an order which we have an id for
+def cancel(order_id):
+    connectionCheck()
+    #Send {"type": "cancel", "order_id": N}
+    pass
